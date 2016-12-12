5553a9e406b03889f7e948e51b6fd10611e6f822
Guillermo Dorado <guillermo.dorado@telefonica.net>
2016-12-12 13:07:10 +0100

    Primera iteración de los menus. Al mirar al suelo aparece un menu in game en 3D para que el usuario interactue. La interacción se hace mirando a los distintos menus. > menuMainController y menuController por separado, hay que revisar.

---
+++ VRProjectLanguage/Assets/Scripts/InGameMenus/menuController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuController : MonoBehaviour {
+
+    private Material material;
+    private Color normal;
+    public Color hightlight;
+    public bool isHover;
+    void Awake ()
+    {
+        material = GetComponent<MeshRenderer>().material;
+        normal = material.color;
+    }
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    public void hover()
+    {
+        material.color = hightlight;
+        isHover = true;
+    }
+
+    public void changeToNormal()
+    {
+        material.color = normal;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!isHover) { changeToNormal(); }
+        isHover = false;
+        
+
+    }
+}
