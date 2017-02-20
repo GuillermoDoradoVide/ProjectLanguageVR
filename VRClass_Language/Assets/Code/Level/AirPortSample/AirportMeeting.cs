88f034140928b13bd5eda75a7c8359808bddd939
Guillermo Dorado Vide <guillermo.dorado@telefonica.net>
2017-02-20 18:10:12 +0100

    Creada la base para la animación del policia y su interacción. Esto va a servir para independizar mejor las animaciones de hablar.

---
+++ VRClass_Language/Assets/Code/Level/AirPortSample/AirportMeeting.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirportMeeting : StateScript {
+
+	public AudioClip[] dialogs;
+	public int currentDialog;
+	public GameObject guard;
+	private DialogScript guardDialogScript;
+	private CharacterAnimationReference characterAnimation;
+
+	// Use this for initialization
+	void Start () {
+		guardDialogScript = guard.GetComponent<DialogScript>();
+		characterAnimation = guard.GetComponent<CharacterAnimationReference> ();
+	}
+
+	// Update is called once per frame
+	public override void atUpdate() {
+		if(!guardDialogScript.playUpdateDialog())
+		{
+			if (currentDialog < dialogs.Length)
+			{
+				EventManager.setNewDialogEvent(dialogs[currentDialog]);
+				guardDialogScript.initDialog();
+				currentDialog++;
+			}
+			else
+			{
+				doChangeThisStateToFinished();
+			}
+		}
+	}
+
+	public override void atInit()
+	{
+		currentDialog = 0;
+		EventManager.setNewDialogEvent(dialogs[currentDialog]);
+		currentDialog++;
+		guardDialogScript.initDialog();
+	}
+
+	public override void atEnd()
+	{
+		//throw new NotImplementedException();
+	}
+
+	public override void atPause()
+	{
+		guardDialogScript.pauseDialog();
+	}
+
+	public override void atReadyActiveState()
+	{
+		guardDialogScript.continueDialog();
+	}
+
+}
