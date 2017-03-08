73b9a05401b672d00ff26bc6f8216da0ffc2ecd0
Guillermo Dorado Vide <guillermo.dorado@telefonica.net>
2017-03-08 10:57:44 +0100

    Implementando el nuevo sistema de menus e interacción.

---
+++ VRClass_Language/Assets/Code/MenuAndInterface/InteractionMenu/IconAnimations.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IconAnimations : MonoBehaviour {
+
+	public RectTransform iconRectTransform;
+	//	public  enum SCALE_ACTION
+	//	{
+	//		SCALE_UP, SCALE_DOWN, SCALE_ORIGIN
+	//	};
+	//	public SCALE_ACTION scaleType;
+	public Vector3 scaleVector = new Vector3(1f, 1f, 1f);
+	public Vector3 scaleOrigin = new Vector3(1f, 1f, 1f);
+	public float scaleSpeed;
+	public float slerpScaleRange;
+
+	private void Awake () {
+		iconRectTransform = GetComponent<RectTransform> ();
+	}
+
+	public void scaleIcon(float factor) {
+		scaleVector.x = factor;
+		scaleVector.y = factor;
+		scaleVector.z = factor;
+		smoothScale ();
+	}
+
+	private void smoothScale() {
+		scaleOrigin = iconRectTransform.localScale;
+		StartCoroutine (calculateScale());
+	}
+
+	private IEnumerator calculateScale()  {
+		slerpScaleRange = 0;
+		while (iconRectTransform.localScale != scaleVector) {
+			iconRectTransform.localScale = Vector3.Slerp (scaleOrigin, scaleVector, slerpScaleRange);
+			slerpScaleRange += Time.deltaTime * scaleSpeed;
+			if (slerpScaleRange > 1)
+				slerpScaleRange = 1;
+			yield return null;
+		}
+	}
+
+}
---
+++ VRClass_Language/Assets/Code/MenuAndInterface/InteractionMenu/panelAlphaController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class panelAlphaController : MonoBehaviour {
+
+	public CanvasGroup canvasGroup;
+	public GameObject canvasGameObject;
+	public float transitionSpeed;
+
+	private void Awake() {
+		canvasGameObject = canvasGroup.gameObject;
+	}
+
+	public void showPanel() {
+		canvasGameObject.SetActive (true);
+		StartCoroutine(showAnimation ());
+	}
+
+	public void hidePanel() {
+		StartCoroutine(hideAnimation ());
+	}
+
+	private IEnumerator showAnimation()
+	{
+		while(canvasGroup.alpha < 1) {
+			canvasGroup.alpha += Time.deltaTime * transitionSpeed;
+			yield return null;
+		}
+		canvasGroup.alpha = 1;
+	}
+
+	private IEnumerator hideAnimation()
+	{
+		while(canvasGroup.alpha > 0) {
+			canvasGroup.alpha -= Time.deltaTime * transitionSpeed;
+			yield return null;
+		}
+		canvasGroup.alpha = 0;
+		canvasGameObject.SetActive (false);
+	}
+}
