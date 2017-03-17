f2270e2d136ae9589133e9311a4bbd622b45f522
Guillermo Dorado Vide <guillermo.dorado@telefonica.net>
2017-03-17 13:33:27 +0100

    Nuevos personajes, nuevos personajes incluidos en la escena. Cambios en el escenario con nueva carteleria y recalculada la iluminación de la escena.

--- VRClass_Language/Assets/Code/Characters/AnimatorLayerScripts/IdleAnimationCycleBehaviour.cs
+++ VRClass_Language/Assets/Code/Characters/AnimatorLayerScripts/IdleAnimationCycleBehaviour.cs
@@ -5,7 +5,7 @@ public class IdleAnimationCycleBehaviour : StateMachineBehaviour {
 
 	  //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		animator.SetInteger("IdleType", Random.Range(0, 3));
+		animator.SetInteger("IdleType", Random.Range(0, 5));
 	}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
