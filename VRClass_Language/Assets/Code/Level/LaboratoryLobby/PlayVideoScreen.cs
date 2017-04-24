a0e1c4a65ba6129f088f176ad321d8500113dbce
Guillermo Dorado Vide <guillermo.dorado@telefonica.net>
2017-04-24 12:13:56 +0200

    Implementada una sencilla función pare reproducir videos como texturas. Creada una textura que simula una pantalla realizando cálculos matemáticos y de programación en la escena del laboratorio.

---
+++ VRClass_Language/Assets/Code/Level/LaboratoryLobby/PlayVideoScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayVideoScreen : MonoBehaviour {
+
+    private Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<Renderer>();
+        ((MovieTexture)renderer.material.mainTexture).loop = true;
+        ((MovieTexture)renderer.material.mainTexture).Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ VRClass_Language/Assets/Code/Level/LaboratoryLobby/rotate_random_floating.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate_random_floating : MonoBehaviour {
+
+	public float _XSPeed;
+	public float _YSPeed;
+	public float _ZSPeed;
+
+	public float _XSRPeed;
+	public float _YSRPeed;
+	public float _ZSRPeed;
+
+	public Transform _T;
+
+	void Awake() {
+		_T = GetComponent<Transform> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		calculatePosition ();
+		calculateRotation ();
+	}
+
+	void calculatePosition () {
+		_T.Translate (_XSPeed * Time.deltaTime, _YSPeed * Time.deltaTime, _ZSPeed * Time.deltaTime);
+	}
+
+	void calculateRotation() {
+
+		_T.Rotate (_XSRPeed * Time.deltaTime, _YSRPeed * Time.deltaTime, _ZSRPeed * Time.deltaTime);
+	
+	}
+}
