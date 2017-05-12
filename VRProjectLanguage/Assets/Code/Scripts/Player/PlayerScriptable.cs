0d65e5544365f4bc02a483193e57f7f6c33829f4
Guillermo Dorado Vide <guillermo.dorado@telefonica.net>
2017-05-12 11:53:34 +0200

    Animaciones de hablar y de la boca del conductor implementadas. Solo falta intentar recalcular sin que pete todo como la otra vez...

--- VRProjectLanguage/Assets/Code/Scripts/Player/PlayerScriptable.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-[CreateAssetMenu(fileName = "PlayerScriptableObject", menuName = "PrefabsManagers/PlayerScriptable", order =3)]
-[System.Serializable]
-public class PlayerScriptable : ScriptableObject {
-    public GameObject menuMainController;
-    public GameObject playerCamera;
-}
