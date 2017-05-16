68b987d731f6a7db646479daec6293a3c58d602f
Guillermo Dorado Vide <guillermo.dorado@telefonica.net>
2017-05-16 11:53:06 +0200

    Nivel base terminado con conversación. Pero toca reducir y evitar giros del coche, así que reharemos la escena entera usando un método de teleport del coche para poder seguir bakeando la iluminación.

---
+++ VRClass_Language/Assets/Code/Level/AirPort/Recogida/Start_driver_conversation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start_driver_conversation : StateScript
+{
+    private void Start() {}
+
+    // Update is called once per frame
+    public override void atUpdate() {}
+
+    public override void atInit()
+    {
+        Debugger.printLog("Init scene: " + SceneController.Instance.getCurrentScene());
+    }
+
+    public override void atEnd() { }
+
+    public override void atPause() {/*throw new NotImplementedException();*/}
+
+    public override void atReadyActiveState() {/*throw new NotImplementedException();*/}
+}
