a96f9aba4d5f3cae11af9fcf26f5feb797fab428
Guillermo Dorado Vide <guillermo.dorado@telefonica.net>
2017-05-16 16:25:17 +0200

    Nueva escena (base) terminada. Ahora a corregir algunas cosas.

--- VRClass_Language/Assets/Code/Level/AirPort/Recogida/CarTeleportReset.cs
+++ VRClass_Language/Assets/Code/Level/AirPort/Recogida/CarTeleportReset.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CarTeleportReset : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Transform origin;
+    public Transform destiny;
+    public Transform car;
+    public float speed;
+    private float t = 0;
+
+    private void Start() {
+        origin.position = new Vector3(origin.position.x, car.position.y, origin.position.z);
+        destiny.position = new Vector3(destiny.position.x, car.position.y, destiny.position.z);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        moveCar();
+    }
+
+    private void moveCar()
+    {
+        car.position = Vector3.Lerp(origin.position, destiny.position, t);
+        calculateT();
+    }
+
+    private void calculateT()
+    {
+        t += speed * Time.deltaTime;
+        if (t > 1)
+        {
+            t = 0;
+        }
+    }
 }
