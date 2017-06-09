d68fdcd3535fa1e2e17953a3442e4dad0440e6a7
Guillermo Dorado Vide <guillermo.dorado@telefonica.net>
2017-06-09 17:12:41 +0200

    Implementado el giro de la cámara con el sensor táctil del controller.

--- VRClass_Language/Assets/Code/Player/PlayerController.cs
+++ VRClass_Language/Assets/Code/Player/PlayerController.cs
@@ -5,6 +5,10 @@ public class PlayerController : MonoBehaviour
 {
 
 	public GameObject menuCanvas;
+    public GameObject mainCamera;
+    public bool touching;
+    public Vector2 firstTouch;
+    public Vector2 currentTouch;
 
 	// Use this for initialization
 	private void Start ()
@@ -13,7 +17,9 @@ public class PlayerController : MonoBehaviour
 		menuCanvas.SetActive (false);
 		EventManager.startListening (Events.EventList.STATE_Pause, activeMenu);
 		EventManager.startListening (Events.EventList.STATE_Continue, disableMenu);
-	}
+        touching = false;
+
+    }
 
 	private void OnEnable() {
 		EventManager.startListening (Events.EventList.STATE_Pause, activeMenu);
@@ -32,5 +38,39 @@ public class PlayerController : MonoBehaviour
 	private void disableMenu() {
 		menuCanvas.SetActive (false);
 	}
+
+    private void Update()
+    {
+        if(GvrController.IsTouching)
+        {
+            Debugger.printLog("touching");
+            if (touching == false)
+            {
+                firstTouch = GvrController.TouchPos;
+                touching = true;
+            }
+            else
+            {
+                currentTouch = GvrController.TouchPos;
+                Debugger.printLog(firstTouch + "<::>" + currentTouch);
+                if (Vector2.Distance(firstTouch, currentTouch) > 0.1f)
+                {
+                    touching = false;
+                    if (firstTouch.x > currentTouch.x)
+                    {
+                        mainCamera.transform.RotateAround(mainCamera.transform.position, Vector3.up, -45);
+                    }
+                    else
+                    {
+                        mainCamera.transform.RotateAround(mainCamera.transform.position, Vector3.up, 45);
+                    }
+                }
+            }
+        }
+        else
+        {
+            touching = false;
+        }
+    }
 
 }
