088f3c0005f206d648db1ab34d3c5e77d355fa4a
Guillermo Dorado Vide <guillermo.dorado@telefonica.net>
2017-07-05 13:30:30 +0200

    [Añadidos] - Revisada la sala del laboratorio. Ahora tiene un botón para cada escena creada hasta el momento y carga correctamena dichas escenas. - Creada una build con todas las escenas (270mb); Se usará a partir de ahora. [Cambios] - Ahora al cambiar entre CUALQUIER escena aparece el fundido negro; antes no estaba implementado en todas las transiciones. (pueden faltar algunas) [Correcciones] - Resueltos los fallos probocados por la falta de vinculación del AnimationController en los personajes con un DialogScript como componente. [Por hacer] - Compresión de texturas, archivos de audio - Organización del proyecto - Historial de versiones de las escenas

--- VRClass_Language/Assets/Code/Level/BackToLobby.cs
+++ VRClass_Language/Assets/Code/Level/BackToLobby.cs
@@ -5,6 +5,14 @@ public class BackToLobby : MonoBehaviour {
 
 	public void backToLobby() {
         SessionManager.Instance.logOutUser();
-		LevelManager.Instance.changeScene ("UserLobby");
+        EventManager.triggerEvent(Events.EventList.PLAYER_FadeOut);
+        LevelManager.Instance.changeScene ("UserLobby");
 	}
+
+    public void BackToLaboratory()
+    {
+        Debugger.printLog("back to lab");
+        EventManager.triggerEvent(Events.EventList.PLAYER_FadeOut);
+        LevelManager.Instance.changeScene("laboratory");
+    }
 }
